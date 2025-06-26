bfac4ed4eeed455e2eeaa166c910593bcaef94f3
Катя Назаревич <katynazarevych@gmail.com>
2025-06-26 20:28:14 +0300

    commit

---
+++ Task Management Exam/DTOs/LoginDto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_Management_Exam.DTOs
+{
+    public class LoginDto
+    {
+        public string Login { get; set; }
+        public string Password { get; set; }
+    }
+}
---
+++ Task Management Exam/DTOs/RegisterDto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_Management_Exam.DTOs;
+
+public class RegisterDto
+{
+    public string Login { get; set; }
+    public string Password { get; set; }
+    public string Email { get; set; }
+}
