f323c25162bb571ed77f124a266df847b63b834b
fezzzyx <marmurartem@gmail.com>
2025-06-26 18:15:39 +0200

    sdf

---
+++ Task Management Exam/Models/User.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_Management_Exam.Models;
+
+public class User
+{
+    public int Id { get; set; }
+    public string Username { get; set; }
+    public string Password { get; set; }
+    public Enums.UserRole Role { get; set; } = Enums.UserRole.Regular;
+}
