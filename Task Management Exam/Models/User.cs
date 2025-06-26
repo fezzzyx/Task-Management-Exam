057194f6412ee96e287cefb6f491b49c8287fac7
fezzzyx <marmurartem@gmail.com>
2025-06-26 18:30:51 +0200

    sdf

--- Task Management Exam/Models/User.cs
+++ Task Management Exam/Models/User.cs
@@ -4,7 +4,6 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-<<<<<<< progress
 namespace Task_Management_Exam.Models;
 
 public class User
@@ -13,11 +12,4 @@ public class User
     public string Username { get; set; }
     public string Password { get; set; }
     public Enums.UserRole Role { get; set; } = Enums.UserRole.Regular;
-=======
-namespace Task_Management_Exam.Models
-{
-    internal class User
-    {
-    }
->>>>>>> master
 }
